81c5a8cacb701f0fa4daaad3995d3de52ac01bd6
Irirwanirira <irijoseph41@gmail.com>
2022-10-11 12:11:05 +0200

    Computed taks n3 with describe method

--- stringLenght.test.js
+++ stringLenght.test.js
@@ -3,3 +3,9 @@ const stringLength = require('./stringLenght')
 test('reflect the length of an array', () => {
     expect(stringLength('length')).toBe(6)
 } )
+
+
+
+test('there is no I in team', () => {
+    expect('team').not.toMatch(/I/);
+} )

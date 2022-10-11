84701062cae7593b7e4b87838a3fff4de829b026
Irirwanirira <irijoseph41@gmail.com>
2022-10-11 12:09:08 +0200

    Created module

---
+++ module/calculus.js
@@ -0,0 +1,19 @@
+const calculator = {
+    add: (a, b) => {
+        return a+b
+    },
+
+    substract: (num1, num2) => {
+        return num1 - num2
+    },
+
+    multiply: (num1, num2) => {
+        return num1 * num2
+    },
+
+    divide: (num1, num2) => {
+        return num1 / num2
+    }
+}
+
+export default calculator
